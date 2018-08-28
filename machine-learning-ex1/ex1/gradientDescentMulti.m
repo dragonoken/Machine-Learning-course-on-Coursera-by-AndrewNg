3523175676bcbb4ef3adcba0e58e4050dc72c1c3
Tyler Yang <0107wy@gmail.com>
2018-08-28 01:35:33 -0700

    Ex 1-3.2 gradient descent (with multiple variables)
    turns out a relatively high learning rate of 1.0 works very good. It started to slow back down at around 1.25 and it diverged when I tried with 1.3 or higher.

--- machine-learning-ex1/ex1/gradientDescentMulti.m
+++ machine-learning-ex1/ex1/gradientDescentMulti.m
@@ -7,6 +7,8 @@ function [theta, J_history] = gradientDescentMulti(X, y, theta, alpha, num_iters
 m = length(y); % number of training examples
 J_history = zeros(num_iters, 1);
 
+fprintf('\n');
+
 for iter = 1:num_iters
 
     % ====================== YOUR CODE HERE ======================
@@ -17,21 +19,25 @@ for iter = 1:num_iters
     %       of the cost function (computeCostMulti) and gradient here.
     %
 
+    delta = (X' * ((X * theta) - y)) / m;
 
+    theta = theta - alpha * delta;
 
+    J = computeCostMulti(X, y, theta);
 
+    if mod(iter, 100) == 0
 
+      fprintf('At Iteration %d\tCost : %0.4f\n', iter, J);
 
-
-
-
-
+    endif
 
     % ============================================================
 
     % Save the cost J in every iteration    
-    J_history(iter) = computeCostMulti(X, y, theta);
+    J_history(iter) = J;
 
 end
+
+fprintf('\n');
 
 end
