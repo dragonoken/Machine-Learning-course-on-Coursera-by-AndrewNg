6bc5fa9134b1a55ed56f70e8a4c921264ff7ed4c
Tyler Yang <0107wy@gmail.com>
2018-08-28 22:48:49 -0700

    Ex 2-2 Regularized Logistic Regression Cost Function and Gradient
    !!! Forgot to mention that the entire exercise 2 is about logistic regression and regularization !!!

--- machine-learning-ex2/ex2/costFunctionReg.m
+++ machine-learning-ex2/ex2/costFunctionReg.m
@@ -17,10 +17,10 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+J = (-y' * log(sigmoid(X * theta)) - (1 - y') * log(1 - sigmoid(X * theta))) / m...
+        + (lambda * ([0; theta(2:end)]' * [0; theta(2:end)])) / (2 * m);
 
-
-
-
+grad = (X' * (sigmoid(X * theta) - y)) / m + (lambda * [0; theta(2:end)]) / m;
 
 % =============================================================
 
