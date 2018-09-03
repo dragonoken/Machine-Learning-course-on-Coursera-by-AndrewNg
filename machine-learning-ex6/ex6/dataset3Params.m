8429d96544f2f50885084375669bef58df62851b
Tyler Yang <0107wy@gmail.com>
2018-09-03 16:44:16 -0700

    Ex 6-1 (Support Vector Machines) - Parameters (C, sigma) for Dataset 3

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -23,11 +23,32 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+Cs = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+n_Cs = numel(Cs);
 
+sigmas = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+n_sigmas = numel(sigmas);
 
+val_errors = zeros(n_Cs * n_sigmas, 1);
 
+fprintf('\n');
+for C_i = 1:n_Cs
+  for sigma_i = 1:n_sigmas
+    fprintf('%d of %d parameter combinations (C : %f, sigma : %f)', ...
+               (n_sigmas * (C_i - 1) + sigma_i), (n_Cs * n_sigmas), Cs(C_i), sigmas(sigma_i));
+    model= svmTrain(X, y, Cs(C_i), @(x1, x2) gaussianKernel(x1, x2, sigmas(sigma_i)));
+    predictions = svmPredict(model, Xval);
+    val_errors(n_sigmas * (C_i - 1) + sigma_i) = mean(double(predictions ~= yval));
+  endfor
+endfor
 
+best_index = find(val_errors == min(val_errors))(1);
 
+C = Cs(floor(best_index / n_sigmas) + 1);
+sigma = sigmas(mod(best_index, n_sigmas));
+
+fprintf('\nBest Parameters : (C : %f, sigma : %f)\n', C, sigma);
+fprintf('Best Error : %f\n', val_errors(best_index));
 
 % =========================================================================
 
